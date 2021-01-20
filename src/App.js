ccb96720206d1a779cc937d8f3d95c93a23fe57e
JAWS-Hub <77670391+JAWS-Hub@users.noreply.github.com>
2021-01-20 12:15:38 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <button>Press me</button>
-const button = document.querySelector('button');
-
-button.onclick = function() {
 class TodoApp extends React.Component {
   constructor(props) {
     super(props);
